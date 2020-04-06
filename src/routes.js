0cf0746364d19fd3bc4fc70140b85ff09f77187d
jean paul <58843994+Hamelet@users.noreply.github.com>
2020-04-06 14:33:54 +0200

    suite TP

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import helloController from "./controllers/helloController";
 const api = ({}) => {
     const routes = Router();
     routes.get('/', helloController.hello);
+    routes.get('/templatedPage', helloController.templatedPage);
     return routes
 };
 
