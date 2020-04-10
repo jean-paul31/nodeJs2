992b55c5e14e20af19dfc9016ba6c7fc940825a4
jean paul <58843994+Hamelet@users.noreply.github.com>
2020-04-10 14:09:43 +0200

    exo db v0.3

--- src/db/userDb.js
+++ src/db/userDb.js
@@ -3,8 +3,19 @@ import DbConnection from "./db";
 const getUsers = async () =>{
     const connexion = new DbConnection();
     return await connexion.performQuery("SELECT * FROM users;", []);
-} 
+};
+const getUser = async (id) =>{
+    const connexion = new DbConnection();
+    return await connexion.performQuery("SELECT * FROM users WHERE id = ?;", [id]);
+};
+const insertUser = async (name, password) =>{
+    const connexion = new DbConnection();
+    return await connexion.performQuery("INSERT INTO users (name, password) VALUES (?, ?);", [name, password]);
+};
+
 
 export default{
-    getUsers
+    getUsers,
+    getUser,
+    insertUser
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import  Router  from "express";
 
 import helloController from "./controllers/helloController";
-import userController from "./controllers/userController"
+import userController from "./controllers/userController";
+import messageController from"./controllers/messageController";
 
 
 const api = () => {
@@ -10,12 +11,15 @@ const api = () => {
     routes.get('/templatedPage', helloController.templatedPage);
 
     routes.get('/users', userController.getUsers);
-
+    routes.put('/user', userController.putUser);
     routes.get('/user/:userId', userController.getUser);
-    routes.post('/user/:userId', userController.postUser);
-    routes.put('/user/:userId', userController.putUser);
+    
+    routes.get('/user/:userId/messages', messageController.getMessageUsers);
+    routes.get('/user/:userId/message', messageController.getMessageUser);
+    routes.put('/message', messageController.putMessageUser);
+    
+    routes.post('/user/:userId', userController.postUser);    
     routes.delete('/user/:userId', userController.deleteUser);
-
     routes.get('/meteo/:city', helloController.getMeteo);
     
     return routes
